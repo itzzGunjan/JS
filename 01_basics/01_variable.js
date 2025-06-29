dc0ccbed805339dc4ef036c8c24c2981e0ea073f
itzzGunjan <115095452+itzzGunjan@users.noreply.github.com>
2025-06-29 13:52:17 +0000

    Learn about Data types in JS

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail="gitu@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+let accountState //undefined
+// accountId =2
+
+accountEmail="hc@.com"
+accountPassword="21212"
+accountCity="BENgalore"
+
+console.log(accountId);
+/*please donot use var
+  because of issue in block scope and functional scope
+*/ 
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // After writing this The JS code will be treated as according to the newer version
+
+// alert (3+3) we are using node.js , and this alert will be used in the browser not on node js
+
+//data types
+// number => 2 to power 53
+//bigint 
+//string
+//boolean => true or false
+//null =>standalone value(its not"undefined ye null h") 
+let state = null
+//undefined => jb variable le liye but value assigned nhi kiye
+// symbol =>unique
+
+//object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);//-->(object) very important iq for interview
+console.log(typeof undefined);//--> (undefined)
+
