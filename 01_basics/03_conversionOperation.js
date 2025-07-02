f85beefbb33984952d06e3f064058791053c8ba4
itzzGunjan <115095452+itzzGunjan@users.noreply.github.com>
2025-07-02 19:14:08 +0000

    Investigate

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="33";
+
+console.log(typeof(score));
+console.log(typeof score);
+
+// to set the string into Number only if it is string also then the method
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// // Exmaple
+// let score = null;
+// console.log(typeof score);
+// console.log(typeof (score);
+
+// //Notees
+// 1. "33" => 33 string can be converted into number
+// 2. "33abc" => NaN (not a number)
+// 3. true =>1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
